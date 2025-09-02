6ef20ece3aca5a2343c37d5d502ae1a10e2f1db4
Christian Bach <christian.bach@scs.ch>
2025-09-02 13:37:00 +0200

    stop the echo command from being stuck in base64 mode

--- app/include/Commands/EchoCommand.h
+++ app/include/Commands/EchoCommand.h
@@ -11,16 +11,18 @@ public:
 
   // Executes the command
   int execute(const std::vector<std::string> &args) override {
+    bool useBase64 = false;
     for (size_t i = 1; i < args.size(); ++i) {
       if (args[i] == "-b64") {
-        _useBase64 = true;
+        useBase64 = true;
         continue;
       }
-      if (_useBase64) {
+      if (useBase64) {
         // If Base64 encoding is enabled, encode the argument
         std::string encoded = Base64::encode(
             std::vector<uint8_t>(args[i].begin(), args[i].end()));
         std::cout << encoded << " ";
+        useBase64 = false;
         continue;
       }
       std::cout << args[i] << " ";
@@ -28,7 +30,4 @@ public:
     std::cout << std::endl;
     return 0; // Return 0 to indicate success
   }
-
-private:
-  bool _useBase64 = false; // Flag to indicate if Base64 encoding is used
 };

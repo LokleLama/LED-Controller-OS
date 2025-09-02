3d5707eac6fa7c5bf939f1ffbae5126b4117f78f
Christian Bach (WSL-Atlas) <christian.bach@scs.ch>
2025-09-02 13:06:02 +0200

    WIP: add Simple Pico File System (SPFS) for the RP2040

--- app/include/Flash/flash.h
+++ app/include/Flash/flash.h
@@ -7,7 +7,10 @@
 
 class Flash {
 public:
-  static int read(std::vector<uint8_t> &buffer, int offset);
-  static int write(const std::vector<uint8_t> &buffer, int offset);
-  static int erase(int offset, int length);
+  static int read(std::vector<uint8_t> &buffer, int address);
+  static void *readPointer(int address);
+  static int write(const std::vector<uint8_t> &buffer, int address);
+  static int erase(int address, int length);
+
+  static constexpr size_t MAX_FLASH_SIZE = 16 * 1024 * 1024;
 };
